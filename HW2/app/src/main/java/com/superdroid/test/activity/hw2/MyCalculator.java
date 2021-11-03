05950e9410c9b073830f629ca746ce6623e0ce11
Sangmin Ju <besforyou2@gmail.com>
2021-11-03 12:36:58 +0900

    first commit

---
+++ HW2/app/src/main/java/com/superdroid/test/activity/hw2/MyCalculator.java
@@ -0,0 +1,14 @@
+package com.superdroid.test.activity.hw2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MyCalculator extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
